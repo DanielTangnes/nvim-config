87a6cfe44d8906640a94eeccc8d8b465d9eb7d69
Daniel Tangnes <66026708+DanielTangnes@users.noreply.github.com>
2026-03-17 15:24:48 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,9 @@ require('config.options')
 require('config.keybinds')
 require('config.lazy')
 require('config.lsp')
+if vim.g.vscode then
+  local vscode = require("vscode")
+  vim.keymap.set("n", "<leader>ff", function() vscode.action("workbench.action.quickOpen") end)
+  vim.keymap.set("n", "<leader>fg", function() vscode.action("workbench.action.findInFiles") end)
+  vim.keymap.set("n", "<leader>fb", function() vscode.action("workbench.action.showAllEditors") end)
+end
