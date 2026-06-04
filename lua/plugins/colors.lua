bb909eac1feb4b5265625ce65363606c99f67d13
Admin - Daniel Tangnes <a_dantan@ihelse.net>
2026-06-04 14:45:31 +0200

    .

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,18 +1,29 @@
 return {
-    {
-      'f4z3r/gruvbox-material.nvim',
-      name = 'gruvbox-material',
-      lazy = false,
-      priority = 1000,
-      opts = {},
-    },
-    {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-	    "nvim-tree/nvim-web-devicons",
+  {
+    'f4z3r/gruvbox-material.nvim',
+    name = 'gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
     },
     opts = {
-	theme = 'gruvbox-material',
+      options = {
+        theme = 'gruvbox-material',
+      },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1, -- 1: Relative path, 2: Absolute path, 3: Absolute path with ~
+          }
+        }
+      }
     }
-    },
+  },
 }
+
