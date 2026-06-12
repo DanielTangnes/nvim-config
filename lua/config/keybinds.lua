2bb15cd137783805ad9750990a4339f52002b840
Admin - Daniel Tangnes <a_dantan@ihelse.net>
2026-06-12 12:30:31 +0200

    .

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,14 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeFindFile)
+-- Find and replace the word under your cursor project-wide
+vim.keymap.set("n", "<leader>fr", function()
+  local grug = require("grug-far")
+  local ext = vim.fn.expand("%:e")
+  grug.open({
+    prefills = {
+      search = vim.fn.expand("<cword>"),
+      filesFilter = ext ~= "" and "*." .. ext or nil,
+    },
+  })
+end, { desc = "Project-wide find and replace (Grug)" })
+
