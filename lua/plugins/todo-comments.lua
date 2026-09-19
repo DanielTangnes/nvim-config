e63003adb63eaf6d195ceb68fdcb576a41be7b66
danieltangnes <danieltangnes1@gmail.com>
2026-09-19 16:42:29 +0200

    Allow todo-comments for MD files

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -4,13 +4,18 @@ return {
       dependencies = { "nvim-lua/plenary.nvim" },
       opts = {
 	highlight = {
+	  comments_only = false,
 	  multiline = true, -- Enable multi-line todo comments
-	  multiline_pattern = "^.", -- Lua pattern to match subsequent lines
+	  multiline_pattern = "^[ \t]*[-*]*[ \t]*", -- Lua pattern to match subsequent lines
 	  multiline_context = 10, -- How many lines to search for multi-line context
 	},
 	-- Optional: define custom keywords or ensure standard ones catch checkboxes
 	keywords = {
-	  TODO = { icon = " ", color = "info" },
+	  TODO = { 
+	      icon = " ",
+	      color = "info",
+	      alt = { "[- [ ]]", "[- [x]]", "TODO:" }
+	  },
 	},
       }
     }
